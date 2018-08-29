3ef53f514eb91a87eefae47e79fd78fff5522b02
courd <courdheadman@gmail.com>
2018-08-29 23:56:11 +0000

    added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const {ObjectID} = require('mongodb')
 const express = require("express")
 const bodyParser = require("body-parser")
 
@@ -28,6 +29,17 @@ app.get('/todos', (req, res) => {
   }), e => {
     res.status(400).send(e)
   }
+})
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  if (!ObjectID.isValid(id)){
+    res.status(404).send("ID not Valid");
+  }
+  Todo.findById(id).then((todo) => {
+    todo ? res.status(200).send({todo}) : res.status(404).send("Id not found")
+  }).catch((e) => res.status(400).send(e))
+
 })
 
 app.listen(3000, () => {
